fb039b72ee004e894a6ca5f422bd7546ddbf82de
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2024-02-28 17:12:12 +0600

    Navbar Unifinished

---
+++ src/components/NavItems.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { useState } from "react";
+
+const NavItems = () => {
+  const [activeIndex, setActiveIndex] = useState<null | number>(null);
+  return <div className="flex gap-4 h-full"></div>;
+};
+
+export default NavItems;
---
+++ src/config/index.ts
@@ -0,0 +1,11 @@
+export const PRODUCT_CATEGORIES = [
+  {
+    label: "UI Kits",
+    value: "ui_kits" as const,
+    featured: [{
+      name: "Editor picks",
+      href: "#",
+      imageSrc: 'nav'
+    }]
+  },
+];

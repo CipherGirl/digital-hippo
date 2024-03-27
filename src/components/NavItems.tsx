2bdc6d4659858efbb001db4e832c01cb3767d517
HasnaHenaMow <hasna.itsd@goldenharvestbd.com>
2024-03-27 12:19:27 +0600

    Navbar closing on click outside or esc down

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,33 @@
 "use client";
 
 import { PRODUCT_CATEGORIES } from "@/config";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavItem from "./NavItem";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", (e) => handler(e));
+
+    return () => {
+      document.removeEventListener("keydown", (e) => handler(e));
+    };
+  }, []);
+
   const isAnyOpen = activeIndex !== null;
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
 
   return (
-    <div className="flex gap-4 h-full">
+    <div className="flex gap-4 h-full" ref={navRef}>
       {PRODUCT_CATEGORIES.map((category, i) => {
         const handleOpen = () => {
           activeIndex === i ? setActiveIndex(null) : setActiveIndex(i);
